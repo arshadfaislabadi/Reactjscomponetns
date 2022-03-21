0b5259e7bb7c769c6c934a35a991df5ece5618fb
arshadfaislabadi <102044245+arshadfaislabadi@users.noreply.github.com>
2022-03-21 20:06:52 +0500

    initital commet

---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from  "react";
+
+
+function List(){
+    return <ol>
+        <li>pakistan</li>
+        <li>India</li>
+        <li>South africa</li>
+    </ol>;
+}
+
+export default List;
