7ef05610046b52b9708bca3e31571d34fbe8ef3c
arshadfaislabadi <102044245+arshadfaislabadi@users.noreply.github.com>
2022-03-22 19:27:12 +0500

    COMPONENT21

--- src/index.js
+++ src/index.js
@@ -1,36 +1,13 @@
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Student from "./Student";
 
-ReactDom.render(
-<App/>,
-document.getElementById("root")
+ReactDOM.render(
+<Student/>,
+  document.getElementById('root')
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
